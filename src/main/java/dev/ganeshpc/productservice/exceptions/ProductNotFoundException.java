09e133cce6cdc5bebded6758ad393c0c256d78ad
Ganesh Chincholkar <gnesh.pc@gmail.com>
2024-02-20 21:21:19 +0530

    One more way to handle exception

--- src/main/java/dev/ganeshpc/productservice/exceptions/ProductNotFoundException.java
+++ src/main/java/dev/ganeshpc/productservice/exceptions/ProductNotFoundException.java
@@ -1,5 +1,12 @@
 package dev.ganeshpc.productservice.exceptions;
 
+// import org.springframework.http.HttpStatus;
+// import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+// Allows to handle exception and status code from here itself
+// @ResponseStatus(value = HttpStatus.NOT_FOUND)
+
 public class ProductNotFoundException extends Exception {
     
     public ProductNotFoundException(String message) {
